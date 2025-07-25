9545d4f846c6404538e8438baca55bda452ba058
JMK <jjsteel8@naver.com>
2025-07-25 18:09:16 +0900

    첫 번째 커밋

---
+++ src/test/java/kr/gg/compick/CompickApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.gg.compick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
